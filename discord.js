63ae404517bb0355caeb757abdeed5580769ace3
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2023-06-13 01:34:08 +0530

    Added event handler for interactions

--- discord.js
+++ discord.js
@@ -28,5 +28,33 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+
+// Event handler for when an interaction occurs (e.g., a slash command)
+client.on(Events.InteractionCreate, async interaction => {
+	// Check if the interaction is a chat input command
+	if (!interaction.isChatInputCommand()) return;
+  
+	// Get the command based on the interaction's command name
+	const command = client.commands.get(interaction.commandName);
+  
+	// If the command does not exist, return
+	if (!command) return;
+  
+	try {
+	  // Execute the command with the interaction as the argument
+	  await command.execute(interaction);
+	} catch (error) {
+	  console.error(error);
+	  // When a message or response is marked as ephemeral, it means that only the user who triggered the interaction
+	  // can see the message. Other users in the same channel or server cannot see the ephemeral message.
+	  if (interaction.replied || interaction.deferred) {
+		await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+	  } else {
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	  }
+	}
+  });
+  
+
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
