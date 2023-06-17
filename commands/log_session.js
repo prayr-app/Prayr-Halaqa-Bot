f1f3dc6389d9d6901a0ad4df54207eacc5f07978
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2023-06-17 15:01:00 +0530

    Created log session command
    The options in the select action are derived from the getAllReciters method in the reciter service

---
+++ commands/log_session.js
@@ -0,0 +1,53 @@
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
+const ReciterService = require('../services/reciter_service');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('log_session')
+        .setDescription('Logs a Quran Session!')
+        .addStringOption(option =>
+            option.setName('surah')
+                .setDescription('What was the last Surah that you recited?')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option.setName('ayah')
+                .setDescription('What verse did you stop at?')
+                .setRequired(true)
+        ),
+        
+    async execute(interaction) {
+        const reciterService = new ReciterService();
+
+        // Retrieve reciters from the service and map them to include only the necessary fields (id and name)
+        let reciters = (await reciterService.getAllReciters())
+            .map(reciter => ({ id: reciter._id, name: reciter.name }));
+
+        console.log(reciters);
+
+        // Adding a new option (StringSelectMenuOptionBuilder) to the reciterOptions arr for each reciter
+        const reciterOptions = [];
+        reciters.forEach((reciter) => {
+            reciterOptions.push(new StringSelectMenuOptionBuilder()
+                .setLabel(reciter.name)
+                .setValue(reciter.id.toString()));
+        });
+
+        // Creating a selection action using StringSelectMenuBuilder
+        const selectReciters = new StringSelectMenuBuilder()
+            .setCustomId('session')
+            .setPlaceholder('Select the reciters')
+            .setMinValues(2)
+            .setMaxValues(10) // Setting a maximum of 10, else the session would be too long
+            .addOptions(reciterOptions);
+
+        const row = new ActionRowBuilder()
+            .addComponents(selectReciters);
+
+        // Reply to the interaction with the content and the row containing the select component
+        await interaction.reply({
+            content: `Who is attending today's Halaqa!`,
+            components: [row],
+        });
+    },
+};
